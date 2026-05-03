d2a9426a54ef16a041643cefac4ef94584530666
Niels de Groot <niels.de.groot.iam@gmail.com>
2026-05-03 12:47:37 +0200

    Sync repo from v92 to v92 2026050313

--- imxweb/projects/qbm/src/lib/about/About.component.ts
+++ imxweb/projects/qbm/src/lib/about/About.component.ts
@@ -71,7 +71,7 @@ export class AboutComponent implements OnInit {
     this.product['Version'] = Globals.Version;
     this.product['Copyright'] = Globals.QBM_Copyright;
     // NS20260424 Update HTML versions
-    this.product['HtmlVersion'] = 'HTML Applications: March 6, 2026';
+    this.product['HtmlVersion'] = 'HTML Applications: April 17, 2026';
     this.product['HtmlCustomVersion'] = 'HTML Customizations: May 3, 2026';
 
     this.product['ThirdPartyLicencesUrl'] = 'https://www.oneidentity.com/legal/third-party-licenses.aspx';
